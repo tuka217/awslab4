552929112d537718d0879a6cf4756adb505db578
Anna Walasek <tuka217@gmail.com>
2017-02-07 20:48:13 +0100

    Lab 4

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -3,9 +3,8 @@ var AWS = require("aws-sdk");
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-
-	
-	
+    var bucket = request.query["bucket"];
+    var key = request.query["key"];
 }
 
 exports.action = task
